f3ee93d36dc4af4ea4212645be05ed302bcabc24
Lxw0628 <761402785@qq.com>
2023-02-11 19:28:32 +0800

    更换分屏快捷键为<M-w>v+h

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -10,11 +10,11 @@ keymap.set("i", "jk", "<ESC>")
 keymap.set("i", "<C-h>", "<LEFT>")
 keymap.set("i", "<C-j>", "<DOWN>")
 keymap.set("i", "<C-k>", "<UP>")
-keymap.set("i", "<C-l>", "<RIGHT>")
+keymap.set("i", "<C-l>", "<RIvimHT>")
 
 -- 单行或多行移动
-keymap.set("i", "<M-j>", "<ESC>:m .+1<CR>i<RIGHT>")
-keymap.set("i", "<M-k>", "<ESC>:m .-2<CR>i<RIGHT>")
+keymap.set("i", "<M-j>", "<ESC>:m .+1<CR>i<RIvimHT>")
+keymap.set("i", "<M-k>", "<ESC>:m .-2<CR>i<RIvimHT>")
 
 -- 快速移动
 keymap.set("i", "<C-M-j>", "<DOWN><DOWN><DOWN><DOWN><DOWN>")
@@ -24,20 +24,22 @@ keymap.set("i", "<C-M-l>", "<END>")
 
 -- ---------- 视觉模式 ---------- ---
 -- keymap.set("v", "jk", "<ESC>")
+keymap.set("v", "<leader>w", ":call v:lua.MagicSave()<CR>", {noremap = true, silent = true})
 
 -- 单行或多行移动
 keymap.set("v", "<M-j>", ":m '>+1<cr>gv", { noremap = true, silent = true })
 keymap.set("v", "<M-k>", ":m '<-2<cr>gv", { noremap = true, silent = true })
-
 -- 快速移动
 keymap.set("v", "<C-M-j>", "5j")
+
 keymap.set("v", "<C-M-k>", "5k")
 keymap.set("v", "<C-M-h>", "0")
 keymap.set("v", "<C-M-l>", "$")
 
 -- ---------- 正常模式 ---------- ---
 -- 保存关闭
-keymap.set("n", "<leader>w", "<ESC>:w<CR>")
+-- keymap.set("n", "<leader>w", "<ESC>:w<CR>")
+keymap.set("n", "<leader>w", ":call v:lua.MagicSave()<cr>", {noremap = true, silent = true})
 keymap.set("n", "<leader>q", "<ESC>:q!<CR>")
 
 -- 单行或多行移动
@@ -46,30 +48,30 @@ keymap.set("n", "<M-k>", ":m .-2<cr>", { noremap = true, silent = true })
 
 -- 折行移动
 keymap.set("n", "j", "gj", {silent=true})
-keymap.set("n", "k", "gk", {silent=true})
 
 -- 快速移动
 keymap.set("n", "<C-M-j>", "5j")
+keymap.set("n", "k", "gk", {silent=true})
 keymap.set("n", "<C-M-k>", "5k")
 keymap.set("n", "<C-M-h>", "0")
 keymap.set("n", "<C-M-l>", "$")
 
 -- 窗口
--- keymap.set("n", "<leader>sv", "<C-w>v") -- 水平新增窗口
--- keymap.set("n", "<leader>sh", "<C-w>s") -- 垂直新增窗口
+keymap.set("n", "<M-w>v", "<C-w>v", {silent = true, nowait = true}) -- 水平新增窗口
+keymap.set("n", "<M-w>h", "<C-w>s") -- 垂直新增窗口
 
 -- 快速滚动居中
 keymap.set("n", "<C-d>", "<C-d>zz")
 keymap.set("n", "<C-u>", "<C-u>zz")
 
 -- 全选/选择
-keymap.set("n", "<M-a>", "ggVG")
+keymap.set("n", "<M-a>", "ggVvim")
 keymap.set("n", "<M-f>", "vif")
-keymap.set("n", "<M-[>", "vi[")
 keymap.set("n", "<M-]>", "vi]")
 keymap.set("n", "<M-(>", "vi(")
 keymap.set("n", "<M-)>", "vi)")
 keymap.set("n", "<M-'>", "vi'")
+keymap.set("n", "<M-[>", "vi[")
 keymap.set("n", "<M-\">", "vi\"")
 
 -- 取消高亮
@@ -107,11 +109,11 @@ vim.api.nvim_command([[
         endif
         try
             if l:up != 0 && l:down != 0
-                exe 'norm! ' . l:up . 'GV' . l:down . 'Gzf'
+                exe 'norm! ' . l:up . 'vimV' . l:down . 'vimzf'
             elseif l:up != 0
-                exe 'norm! V' . l:up . 'Gzf'
+                exe 'norm! V' . l:up . 'vimzf'
             elseif l:down != 0
-                exe 'norm! V' . l:down . 'Gzf'
+                exe 'norm! V' . l:down . 'vimzf'
             else
                 exe 'norm! za'
             endif
@@ -120,6 +122,15 @@ vim.api.nvim_command([[
         endtry
     endf
 ]])
+
+function MagicSave()
+    if vim.fn.empty(vim.fn.glob(vim.fn.expand('%:p:h'))) then vim.fn.system('mkdir -p ' .. vim.fn.expand('%:p:h')) end
+    if vim.o.buftype == 'acwrite' then
+        vim.fn.execute('w !sudo tee > /dev/null %')
+    else
+        vim.fn.execute('w')
+    end
+end
 
 -- ---------- 插件 ---------- ---
 -- nvim-tree
