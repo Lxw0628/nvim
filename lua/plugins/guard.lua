aa38916aea3be580a2460a5523987115b7fb4fe6
Lxw0628 <761402785@qq.com>
2023-08-16 16:13:37 +0800

    use guard.nvim to format code

--- lua/plugins/guard.lua
+++ lua/plugins/guard.lua
@@ -8,7 +8,7 @@ return {
         ft("python"):fmt("black"):append("isort")
         ft("json"):fmt("jq")
         ft("sh"):fmt("shfmt"):lint("shellcheck")
-        ft("html"):fmt("prettier"):list("markuplint")
+        ft("html"):fmt("prettier"):lint("markuplint")
         ft("css,sass,scss,less"):fmt("lsp"):append("prettier"):lint("stylelint")
         ft("javascript,typescript"):fmt("prettier"):lint("eslint_d")
 
