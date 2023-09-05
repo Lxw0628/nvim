6f5e7f9a1dab66fe8f8ae524a960db0312cba8ce
Lxw0628 <761402785@qq.com>
2023-09-05 15:16:32 +0800

    prettier空格缩进更改为4

--- lua/plugins/guard.lua
+++ lua/plugins/guard.lua
@@ -1,21 +1,37 @@
 return {
-    "nvimdev/guard.nvim",
-    event = "VeryLazy",
-    config = function()
-        local ft = require("guard.filetype")
+	"nvimdev/guard.nvim",
+	event = "VeryLazy",
+	config = function()
+		local ft = require("guard.filetype")
 
-        ft("lua"):fmt("lsp"):append("stylua")
-        ft("python"):fmt("black"):append("isort")
-        ft("json"):fmt("jq")
-        ft("sh"):fmt("shfmt")
-        ft("html"):fmt("prettier")
-        ft("css,sass,scss,less"):fmt("lsp"):append("prettier")
-        ft("javascript,typescript"):fmt("prettier")
-        ft("markdown"):fmt("cbfmt")
-
-        require("guard").setup({
-            fmt_on_save = false,
-            lsp_as_default_formatter = true,
+		ft("lua"):fmt("lsp"):append("stylua")
+		ft("python"):fmt("black"):append("isort")
+		ft("json"):fmt("jq")
+		ft("sh"):fmt("shfmt")
+		ft("html"):fmt({
+			cmd = "prettier",
+			args = { "--stdin-filepath", "--tab-width", "4" },
+			stdin = true,
+			fname = true,
+		})
+		ft("css,sass,scss,less"):fmt("lsp"):append({
+            cmd = "prettier",
+			args = { "--stdin-filepath", "--tab-width", "4" },
+			stdin = true,
+			fname = true,
         })
-    end,
+		ft("javascript,typescript"):fmt({
+            cmd = "prettier",
+			args = { "--stdin-filepath", "--tab-width", "4" },
+			stdin = true,
+			fname = true,
+        })
+
+		ft("markdown"):fmt("cbfmt")
+
+		require("guard").setup({
+			fmt_on_save = false,
+			lsp_as_default_formatter = true,
+		})
+	end,
 }
