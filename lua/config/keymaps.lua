9c8415d8138601a056be02cbe5828fb80b2cdea9
Lxw0628 <761402785@qq.com>
2023-10-29 22:39:55 +0800

    finish

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,25 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+-- quick ESC
+vim.keymap.set("i", "jk", "<ESC>")
+-- quick move
+vim.keymap.set("n", "<C-M-h>", "0")
+vim.keymap.set("n", "<C-M-j>", "5j")
+vim.keymap.set("n", "<C-M-k>", "5k")
+vim.keymap.set("n", "<C-M-l>", "$")
+vim.keymap.set("i", "<C-M-h>", "<HOME>")
+vim.keymap.set("i", "<C-M-j>", "<DOWN><DOWN><DOWN><DOWN><DOWN>")
+vim.keymap.set("i", "<C-M-k>", "<UP><UP><UP><UP><UP>")
+vim.keymap.set("i", "<C-M-l>", "<END>")
+vim.keymap.set("v", "<C-M-h>", "0")
+vim.keymap.set("v", "<C-M-j>", "5j")
+vim.keymap.set("v", "<C-M-k>", "5k")
+vim.keymap.set("v", "<C-M-l>", "$")
+-- move cursor in insert mode
+vim.keymap.set("i", "<C-k>", "<Nop>")
+vim.keymap.set("i", "<C-h>", "<LEFT>")
+vim.keymap.set("i", "<C-k>", "<UP>")
+vim.keymap.set("i", "<C-j>", "<DOWN>")
+vim.keymap.set("i", "<C-l>", "<RIGHT>")
---
+++ lua/plugins/change.lua
@@ -0,0 +1,132 @@
+return {
+  {
+    "neovim/nvim-lspconfig",
+    init = function()
+      local keys = require("lazyvim.plugins.lsp.keymaps").get()
+      -- change a keymap
+      -- keys[#keys + 1] = { "K", "<cmd>echo 'hello'<cr>" }
+      -- disable a keymap
+      -- keys[#keys + 1] = { "K", false }
+      -- add a keymap
+      -- keys[#keys + 1] = { "H", "<cmd>echo 'hello'<cr>" }
+      keys[#keys + 1] = { "<C-k>", false }
+    end,
+  },
+  {
+    "echasnovski/mini.surround",
+    opts = {
+      mappings = {
+        add = "sa",
+        delete = "sd",
+        find = "sf",
+        find_left = "sF",
+        highlight = "sh",
+        replace = "sr",
+        update_n_lines = "sn",
+      },
+    },
+  },
+  {
+    "nvim-telescope/telescope.nvim",
+    keys = {
+      -- disable the keymap to grep files
+      { "<leader>/", false },
+      -- change a keymap
+      { "<leader>ff", "<cmd>Telescope find_files<cr>", desc = "Find Files" },
+      -- add a keymap to browse plugin files
+      {
+        "<leader>fp",
+        function()
+          require("telescope.builtin").find_files({ cwd = require("lazy.core.config").options.root })
+        end,
+        desc = "Find Plugin File",
+      },
+    },
+  },
+  {
+    "folke/flash.nvim",
+    keys = {
+      -- disable the default flash keymap
+      { "s", mode = { "n", "x", "o" }, false },
+      { "S", mode = { "n", "x", "o" }, false },
+    },
+  },
+  {
+    "nvim-treesitter/nvim-treesitter-context",
+    opts = {
+      max_lines = 1,
+    },
+  },
+  {
+    "telescope.nvim",
+    dependencies = {
+      "nvim-telescope/telescope-fzf-native.nvim",
+      build = "make",
+      config = function()
+        require("telescope").load_extension("fzf")
+      end,
+    },
+  },
+  {
+    {
+      "hrsh7th/nvim-cmp",
+      dependencies = { "hrsh7th/cmp-emoji" },
+      ---@param opts cmp.ConfigSchema
+      opts = function(_, opts)
+        local cmp = require("cmp")
+        opts.sources = cmp.config.sources(vim.list_extend(opts.sources, { { name = "emoji" } }))
+      end,
+    },
+  },
+  -- Use <tab> for completion and snippets (supertab)
+  -- first: disable default <tab> and <s-tab> behavior in LuaSnip
+  {
+    "L3MON4D3/LuaSnip",
+    keys = function()
+      return {}
+    end,
+  },
+  -- then: setup supertab in cmp
+  {
+    "hrsh7th/nvim-cmp",
+    dependencies = {
+      "hrsh7th/cmp-emoji",
+    },
+    ---@param opts cmp.ConfigSchema
+    opts = function(_, opts)
+      local has_words_before = function()
+        unpack = unpack or table.unpack
+        local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+        return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+      end
+
+      local luasnip = require("luasnip")
+      local cmp = require("cmp")
+
+      opts.mapping = vim.tbl_extend("force", opts.mapping, {
+        ["<Tab>"] = cmp.mapping(function(fallback)
+          if cmp.visible() then
+            cmp.select_next_item()
+            -- You could replace the expand_or_jumpable() calls with expand_or_locally_jumpable()
+            -- this way you will only jump inside the snippet region
+          elseif luasnip.expand_or_jumpable() then
+            luasnip.expand_or_jump()
+          elseif has_words_before() then
+            cmp.complete()
+          else
+            fallback()
+          end
+        end, { "i", "s" }),
+        ["<S-Tab>"] = cmp.mapping(function(fallback)
+          if cmp.visible() then
+            cmp.select_prev_item()
+          elseif luasnip.jumpable(-1) then
+            luasnip.jump(-1)
+          else
+            fallback()
+          end
+        end, { "i", "s" }),
+      })
+    end,
+  },
+}
