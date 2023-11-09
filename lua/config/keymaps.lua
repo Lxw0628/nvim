215b0e38407fe31915f209340532010834570943
Lxw0628 <761402785@qq.com>
2023-11-09 15:45:02 +0800

    <leader>uu Toggle codeium

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -24,3 +24,27 @@ vim.keymap.set("i", "<C-k>", "<UP>")
 vim.keymap.set("i", "<C-j>", "<DOWN>")
 vim.keymap.set("i", "<C-l>", "<RIGHT>")
 
+vim.keymap.set("n", "<leader>uu", function()
+  local cmp = require("cmp")
+  local sources = cmp.get_config().sources
+  local function isCodeiumEnabled()
+    for _, source in ipairs(sources) do
+      if source.name == "codeium" then
+        return true
+      end
+    end
+    return false
+  end
+  for i = #sources, 1, -1 do
+    if isCodeiumEnabled() then
+      if sources[i].name == "codeium" then
+        table.remove(sources, i)
+        vim.notify("Disabled codeium")
+      end
+    else
+      sources[#sources + 1] = { name = "codeium", group_index = 1, priority = 100 }
+        vim.notify("Enabled codeium")
+    end
+  end
+  cmp.setup.buffer({ sources = sources })
+end, { desc = "Toggle codeium" })
