63225b7eb1000e8d37fe17ceac78a308ba14454e
Lxw0628 <761402785@qq.com>
2024-04-21 16:56:31 +0800

    upload

--- init.lua
+++ init.lua
@@ -1,2 +1,9 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
-require("config.lazy")
+
+if vim.g.vscode then
+  -- VSCode extension
+  -- vim.keymap.set("n", "H", '<cmd>lua require("vscode-neovim").action("workbench.action.previousEditor")<cr>', {})
+  -- vim.keymap.set("n", "L", '<cmd>lua require("vscode-neovim").action("workbench.action.nextEditor")<cr>', {})
+else
+  require("config.lazy")
+end
