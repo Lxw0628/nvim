3fdb6fc23732a1ac9800d43916bd44260625f33c
Lxw0628 <761402785@qq.com>
2024-07-22 03:13:29 +0800

    add cmp.lua file: setting something in this file
    add some cmp sourth and change keybinds.

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -1,17 +1,17 @@
+local function list_index(t, value)
+  for i, v in ipairs(t) do
+    if v == value then
+      return i
+    end
+  end
+  return nil
+end
+
 return {
   {
     "hrsh7th/nvim-cmp",
     version = false, -- last release is way too old
     event = "InsertEnter",
-    dependencies = {
-      "hrsh7th/cmp-nvim-lsp",
-      "hrsh7th/cmp-buffer",
-      "hrsh7th/cmp-path",
-      "hrsh7th/cmp-calc",
-      "hrsh7th/cmp-emoji",
-      "jc-doyle/cmp-pandoc-references",
-      "kdheepak/cmp-latex-symbols",
-    },
     -- Not all LSP servers add brackets when completing a function.
     -- To better deal with this, LazyVim adds a custom option to cmp,
     -- that you can configure. For example:
@@ -21,155 +21,202 @@ return {
     --   auto_brackets = { "python" }
     -- }
     -- ```
-    opts = function()
+    opts = function(_, opts)
       vim.api.nvim_set_hl(0, "CmpGhostText", { link = "Comment", default = true })
       local cmp = require("cmp")
       local luasnip = require("luasnip")
-      -- local defaults = require("cmp.config.default")()
-      local compare = require("cmp.config.compare")
-
-      local auto_select = true
       local function has_words_before()
         local line, col = unpack(vim.api.nvim_win_get_cursor(0))
         return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
       end
-      return {
-        auto_brackets = {}, -- configure any filetype to auto add brackets
-        completion = {
-          completeopt = "menu,menuone,noinsert" .. (auto_select and "" or ",noselect"),
-        },
-        preselect = auto_select and cmp.PreselectMode.Item or cmp.PreselectMode.None,
-        mapping = cmp.mapping.preset.insert({
-          ["<C-b>"] = cmp.mapping.scroll_docs(-4),
-          ["<C-f>"] = cmp.mapping.scroll_docs(4),
-          ["<C-Space>"] = cmp.mapping.complete(),
-          -- ["<CR>"] = LazyVim.cmp.confirm({ select = auto_select }),
-          ["<CR>"] = cmp.config.disable,
-          ["<C-N>"] = cmp.mapping(function()
-            if luasnip.jumpable(1) then
-              luasnip.jump(1)
-            end
-          end, { "i", "c" }),
-          ["<C-P>"] = cmp.mapping(function()
-            if luasnip.jumpable(-1) then
-              luasnip.jump(-1)
-            end
-          end, { "i", "c" }),
-          -- ["<C-y>"] = LazyVim.cmp.confirm({ select = true }),
-          -- ["<S-CR>"] = LazyVim.cmp.confirm({ behavior = cmp.ConfirmBehavior.Replace }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
-          -- ["<C-CR>"] = function(fallback)
-          --   cmp.abort()
-          --   fallback()
-          -- end,
-          ["<C-j>"] = cmp.mapping.select_next_item(),
-          ["<C-k>"] = cmp.mapping.select_prev_item(),
-          ["<Tab>"] = cmp.mapping(function(fallback)
-            if true then
-              if cmp.visible() then
-                cmp.confirm()
-              else
-                fallback()
-              end
+      opts.mapping = cmp.mapping.preset.insert({
+        ["<C-b>"] = cmp.mapping.scroll_docs(-1),
+        ["<C-f>"] = cmp.mapping.scroll_docs(1),
+        ["<C-Space>"] = cmp.mapping.complete(),
+        -- ["<CR>"] = LazyVim.cmp.confirm({ select = auto_select }),
+        ["<CR>"] = cmp.config.disable,
+        ["<C-N>"] = cmp.mapping(function(fallback)
+          if luasnip.jumpable(1) then
+            luasnip.jump(1)
+          else
+            fallback()
+          end
+        end, { "i", "c" }),
+        ["<C-P>"] = cmp.mapping(function(fallback)
+          if luasnip.jumpable(-1) then
+            luasnip.jump(-1)
+          else
+            fallback()
+          end
+        end, { "i", "c" }),
+        -- ["<C-y>"] = LazyVim.cmp.confirm({ select = true }),
+        -- ["<S-CR>"] = LazyVim.cmp.confirm({ behavior = cmp.ConfirmBehavior.Replace }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+        -- ["<C-CR>"] = function(fallback)
+        --   cmp.abort()
+        --   fallback()
+        -- end,
+        ["<C-j>"] = cmp.mapping.select_next_item(),
+        ["<C-k>"] = cmp.mapping.select_prev_item(),
+        ["<Tab>"] = cmp.mapping(function(fallback)
+          if true then
+            if cmp.visible() then
+              cmp.confirm()
             else
-              if cmp.visible() and has_words_before() then
-                cmp.confirm({ select = true })
-              else
-                fallback()
-              end
+              fallback()
             end
-          end, { "i", "c" }),
-        }),
-        sources = cmp.config.sources({
-          {
-            name = "nvim_lsp",
-            ---@param entry cmp.Entry
-            ---@param ctx cmp.Context
-            entry_filter = function(entry, ctx)
-              -- Check if the buffer type is 'vue'
-              if ctx.filetype ~= "vue" then
-                return true
-              end
-
-              local cursor_before_line = ctx.cursor_before_line
-              -- For events
-              if cursor_before_line:sub(-1) == "@" then
-                return entry.completion_item.label:match("^@")
-              -- For props also exclude events with `:on-` prefix
-              elseif cursor_before_line:sub(-1) == ":" then
-                return entry.completion_item.label:match("^:") and not entry.completion_item.label:match("^:on-")
-              else
-                return true
-              end
-            end,
-            priority = 1000,
-          },
-          { name = "luasnip", priority = 750 },
-          { name = "pandoc_references", priority = 725 },
-          { name = "latex_symbols", priority = 700 },
-          { name = "emoji", priority = 700 },
-          { name = "calc", priority = 650 },
-          { name = "path", priority = 500 },
-          { name = "buffer", priority = 250 },
-        }),
-        formatting = {
-          format = function(entry, item)
-            local icons = LazyVim.config.icons.kinds
-            if icons[item.kind] then
-              item.kind = icons[item.kind] .. item.kind
+          else
+            if cmp.visible() and has_words_before() then
+              cmp.confirm({ select = true })
+            else
+              fallback()
             end
+          end
+        end, { "i", "c" }),
+      })
+    end,
+    main = "lazyvim.util.cmp",
+  },
 
-            local widths = {
-              abbr = vim.g.cmp_widths and vim.g.cmp_widths.abbr or 40,
-              menu = vim.g.cmp_widths and vim.g.cmp_widths.menu or 30,
-            }
-
-            for key, width in pairs(widths) do
-              if item[key] and vim.fn.strdisplaywidth(item[key]) > width then
-                item[key] = vim.fn.strcharpart(item[key], 0, width - 1) .. "…"
-              end
-            end
-
-            return item
-          end,
-        },
-        experimental = {
-          ghost_text = {
-            hl_group = "CmpGhostText",
+  {
+    "hrsh7th/nvim-cmp",
+    optional = true,
+    dependencies = { "hrsh7th/cmp-calc", lazy = true },
+    opts = function(_, opts)
+      opts.sources = opts.sources or {}
+      table.insert(opts.sources, { name = "calc" })
+    end,
+  },
+  {
+    "hrsh7th/cmp-cmdline",
+    keys = { ":", "/", "?" }, -- lazy load cmp on more keys along with insert mode
+    dependencies = { "hrsh7th/nvim-cmp" },
+    opts = function()
+      local cmp = require("cmp")
+      return {
+        {
+          type = "/",
+          mapping = cmp.mapping.preset.cmdline(),
+          sources = {
+            { name = "buffer" },
           },
         },
-        -- sorting = defaults.sorting,
-        sorting = {
-          comparators = {
-            compare.offset,
-            compare.exact,
-            compare.score,
-            compare.recently_used,
-            function(entry1, entry2)
-              local _, entry1_under = entry1.completion_item.label:find("^_+")
-              local _, entry2_under = entry2.completion_item.label:find("^_+")
-              entry1_under = entry1_under or 0
-              entry2_under = entry2_under or 0
-              if entry1_under > entry2_under then
-                return false
-              elseif entry1_under < entry2_under then
-                return true
-              end
-            end,
-            compare.kind,
-            compare.sort_text,
-            compare.length,
-            compare.order,
-          },
-        },
-        window = {
-          completion = {
-            col_offset = 1,
-            side_padding = 1,
-            scrollbar = false,
-          },
+        {
+          type = ":",
+          mapping = cmp.mapping.preset.cmdline(),
+          sources = cmp.config.sources({
+            { name = "path" },
+          }, {
+            {
+              name = "cmdline",
+              option = {
+                ignore_cmds = { "Man", "!" },
+              },
+            },
+          }),
         },
       }
     end,
-    main = "lazyvim.util.cmp",
+    config = function(_, opts)
+      local cmp = require("cmp")
+      vim.tbl_map(function(val)
+        cmp.setup.cmdline(val.type, val)
+      end, opts)
+    end,
+  },
+  {
+    "hrsh7th/nvim-cmp",
+    optional = true,
+    dependencies = { "hrsh7th/cmp-emoji" },
+    opts = function(_, opts)
+      if not opts.sources then
+        opts.sources = {}
+      end
+      table.insert(opts.sources, { name = "emoji", priority = 700 })
+    end,
+  },
+  -- {
+  --   "petertriho/cmp-git",
+  --   ft = { "gitcommit", "octo", "NeogitCommitMessage" },
+  --   dependencies = { "hrsh7th/nvim-cmp" },
+  --   opts = function(_, opts)
+  --     opts.filetypes = require("lazy.core.plugin").values(assert(require("astrocore").get_plugin("cmp-git")), "ft")
+  --   end,
+  --   config = function(_, opts)
+  --     if opts.filetypes then
+  --       local cmp = require("cmp")
+  --       cmp.setup.filetype(opts.filetypes, {
+  --         sources = cmp.config.sources({
+  --           { name = "git" },
+  --         }, {
+  --           { name = "buffer" },
+  --         }),
+  --       })
+  --     end
+  --     require("cmp_git").setup(opts)
+  --   end,
+  -- },
+  {
+    "hrsh7th/nvim-cmp",
+    optional = true,
+    dependencies = { "hrsh7th/cmp-latex-symbols" },
+    opts = function(_, opts)
+      if not opts.sources then
+        opts.sources = {}
+      end
+      table.insert(opts.sources, { name = "latex_symbols", priority = 700 })
+    end,
+  },
+  {
+    "hrsh7th/nvim-cmp",
+    optional = true,
+    dependencies = { "chrisgrieser/cmp-nerdfont" },
+    opts = function(_, opts)
+      if not opts.sources then
+        opts.sources = {}
+      end
+      table.insert(opts.sources, { name = "nerdfont", priority = 700 })
+    end,
+  },
+  {
+    "hrsh7th/nvim-cmp",
+    optional = true,
+    dependencies = { "hrsh7th/cmp-nvim-lua", lazy = true },
+    opts = function(_, opts)
+      if not opts.sources then
+        opts.sources = {}
+      end
+      table.insert(opts.sources, { name = "nvim_lua" })
+    end,
+  },
+  {
+    "hrsh7th/nvim-cmp",
+    optional = true,
+    dependencies = { "f3fora/cmp-spell", lazy = true },
+    opts = function(_, opts)
+      opts.sources = opts.sources or {}
+      table.insert(opts.sources, { name = "spell" })
+    end,
+  },
+  {
+    "hrsh7th/nvim-cmp",
+    optional = true,
+    dependencies = { "lukas-reineke/cmp-under-comparator", lazy = true },
+    opts = function(_, opts)
+      local cmp = require("cmp")
+      opts.sorting = opts.sorting or {}
+      -- If comparators is not set, use the default. get_config() has defaults now.
+      opts.sorting.comparators = opts.sorting.comparators or cmp.get_config().sorting.comparators
+      -- Find element in comparators we will position ourselves after.
+      -- Position after recently_used, fallback to after score, fallback to 4th position.
+      -- recently_used was not in nvim-cmp some time ago.
+      local pos = list_index(opts.sorting.comparators, cmp.config.compare.recently_used)
+      if pos == nil then
+        pos = list_index(opts.sorting.comparators, cmp.config.compare.score)
+      end
+      if pos == nil then
+        pos = 3
+      end
+      table.insert(opts.sorting.comparators, pos + 1, require("cmp-under-comparator").under)
+    end,
   },
 }
