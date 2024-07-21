83383d5aa77fe27cd28ce4df396adc9d874b128a
Lxw0628 <761402785@qq.com>
2024-07-22 03:12:36 +0800

    add lsp.lua file: settings something in this file.

---
+++ lua/plugins/lsp.lua
@@ -0,0 +1,13 @@
+return {
+  -- Fix clangd offset encodings
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      setup = {
+        clangd = function(_, opts)
+          opts.capabilities.offsetEncoding = { "utf-16" }
+        end,
+      },
+    },
+  },
+}
