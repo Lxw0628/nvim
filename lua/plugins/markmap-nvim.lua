5440651436f1b34ace1936fcd484437dbcd5e38b
Lxw0628 <761402785@qq.com>
2024-07-22 12:32:21 +0800

    add plugin: markmap-nvim

---
+++ lua/plugins/markmap-nvim.lua
@@ -0,0 +1,28 @@
+--  [markdown markmap]
+--  https://github.com/Zeioth/markmap.nvim
+return {
+  "Zeioth/markmap.nvim",
+  build = "npm -g i markmap-cli",
+  cmd = { "MarkmapOpen", "MarkmapSave", "MarkmapWatch", "MarkmapWatchStop" },
+  ft = "markdown",
+  opts = {
+    hide_toolbar = "false",
+  },
+  specs = {
+    {
+      "jay-babu/mason-null-ls.nvim",
+      event = { "BufReadPre", "BufNewFile" },
+      dependencies = {
+        "williamboman/mason.nvim",
+        "nvimtools/none-ls.nvim",
+      },
+      opts = function()
+        require("mason-null-ls").setup({
+          ensure_installed = { "markmap-cli" },
+          automatic_installation = true,
+          handlers = {},
+        })
+      end,
+    },
+  },
+}
