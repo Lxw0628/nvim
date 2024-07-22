f03329fed3ce1361b419d4ee38aec0c998b263a1
Lxw0628 <761402785@qq.com>
2024-07-22 09:53:44 +0800

    add plugin: nvim-context-vt.lua
    <leader>uv toggle show.

---
+++ lua/plugins/nvim-context-vt.lua
@@ -0,0 +1,10 @@
+return {
+  "andersevenrud/nvim_context_vt",
+  event = "VeryLazy",
+  cmd = { "NvimContextVtToggle" },
+  opts = function()
+    vim.keymap.set("n", "<Leader>uv", function()
+      require("nvim_context_vt").toggle()
+    end, { desc = "Toggle virutal text context" })
+  end,
+}
