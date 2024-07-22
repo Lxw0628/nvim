128f02e2ee0c730297f0e2bbe1e15ffe55aa6fe0
Lxw0628 <761402785@qq.com>
2024-07-22 12:06:37 +0800

    fix: lsp_lines keybind

--- lua/plugins/lsp_lines-nvim.lua
+++ lua/plugins/lsp_lines-nvim.lua
@@ -2,13 +2,14 @@ return {
   "https://git.sr.ht/~whynothugo/lsp_lines.nvim",
   event = "LspAttach",
   dependencies = {},
-  config = function()
-    vim.diagnostic.config({
-      virtual_text = false,
-    })
-
+  init = function()
     vim.keymap.set("n", "<Leader>uD", function()
       require("lsp_lines").toggle()
     end, { desc = "Toggle virtual diagnostic lines" })
   end,
+  opts = function ()
+    vim.diagnostic.config({
+      virtual_text = false,
+    })
+  end
 }
