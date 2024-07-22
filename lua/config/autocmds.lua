ade2e47c492b6d7e4f5c2c97fe3e86060e670755
Lxw0628 <761402785@qq.com>
2024-07-22 23:05:41 +0800

    leetcode start in VimEnter arg change to 'leetcode.nvim'

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -7,7 +7,7 @@ vim.api.nvim_create_autocmd({ "VimEnter" }, {
   nested = true,
   callback = function()
     if vim.fn.argc() ~= 1 then return end -- return if more than one argument given
-    local arg = "leetcode"
+    local arg = "leetcode.nvim"
     if vim.fn.argv()[1] ~= arg then return end -- return if argument doesn't match trigger
     local lines = vim.api.nvim_buf_get_lines(0, 0, -1, true)
     if #lines > 1 or (#lines == 1 and lines[1]:len() > 0) then return end -- return if buffer is non-empty
