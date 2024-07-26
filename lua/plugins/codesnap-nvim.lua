14880206350dfcfe4eaf1966f8f2fef2b3faca2b
Lxw0628 <761402785@qq.com>
2024-07-26 08:04:01 +0800

    修改codesnap一些配置

--- lua/plugins/codesnap-nvim.lua
+++ lua/plugins/codesnap-nvim.lua
@@ -4,20 +4,29 @@ return {
   build = "make",
   cmd = { "CodeSnap", "CodeSnapSave", "CodeSnapHighlight", "CodeSnapSaveHighlight" },
   init = function()
-    vim.keymap.set("v", prefix, "", { desc = " CodeSnap" })
-    vim.keymap.set("v", prefix .. "s", ":'<,'>CodeSnap<CR>", { desc = "CodeSnap (clipboard)" })
-    vim.keymap.set("v", prefix .. "S", ":'<,'>CodeSnapSave<CR>", { desc = "CodeSnap (save)" })
-    vim.keymap.set("v", prefix .. "a", ":'<,'>CodeSnapASCII<CR>", { desc = "CodeSnap (ASCII)" })
-    vim.keymap.set("v", prefix .. "h", ":'<,'>CodeSnapHighlight<CR>", { desc = "CodeSnap with highlight (clipboard)" })
-    vim.keymap.set("v", prefix .. "H", ":'<,'>CodeSnapSaveHighlight<CR>", { desc = "CodeSnap with highlight (save)" })
+    vim.keymap.set("x", prefix, "", { desc = " CodeSnap" })
+    vim.keymap.set("x", prefix .. "s", ":'<,'>CodeSnap<CR>", { desc = "CodeSnap (clipboard)" })
+    vim.keymap.set("x", prefix .. "S", ":'<,'>CodeSnapSave<CR>", { desc = "CodeSnap (save)" })
+    vim.keymap.set("x", prefix .. "a", ":'<,'>CodeSnapASCII<CR>", { desc = "CodeSnap (ASCII)" })
+    vim.keymap.set("x", prefix .. "h", ":'<,'>CodeSnapHighlight<CR>", { desc = "CodeSnap with highlight (clipboard)" })
+    vim.keymap.set("x", prefix .. "H", ":'<,'>CodeSnapSaveHighlight<CR>", { desc = "CodeSnap with highlight (save)" })
   end,
   opts = {
-    save_path = "~/Pictures/Codesnap",
-    mac_window_bar = false,
-    has_breadcrumbs = false,
-    has_line_number = true,
-    bg_color = "#1e1e2e",
-    bg_padding = 0,
+    mac_window_bar = true,
+    title = "CodeSnap.nvim",
+    code_font_family = "Maple Mono SC NF",
+    watermark_font_family = "Maple Mono SC NF",
     watermark = "",
+    bg_theme = "bamboo",
+    has_breadcrumbs = false,
+    breadcrumbs_separator = "/",
+    has_line_number = true,
+    show_workspace = false,
+    min_width = 0,
+    bg_x_padding = 20,
+    bg_y_padding = 20,
+    bg_padding = nil,
+    bg_color = "#535c68",
+    save_path = "~/Pictures/Codesnap",
   },
 }
