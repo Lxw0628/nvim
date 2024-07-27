347898e47860c3b7c43c6675ebf62d35e52c1e4f
Lxw0628 <761402785@qq.com>
2024-07-27 14:05:22 +0800

    删除了自定的cmp sources 的优先级

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -17,8 +17,7 @@ return {
       --   unpack = unpack or table.unpack
       --   local line, col = unpack(vim.api.nvim_win_get_cursor(0))
       --   return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
-      -- end
-      local cmp = require("cmp")
+      -- end local cmp = require("cmp")
       opts.mapping = vim.tbl_extend("force", opts.mapping, {
         ["<C-b>"] = cmp.mapping.scroll_docs(-1),
         ["<C-f>"] = cmp.mapping.scroll_docs(1),
@@ -98,7 +97,7 @@ return {
       if not opts.sources then
         opts.sources = {}
       end
-      table.insert(opts.sources, { name = "emoji", priority = 700 })
+      table.insert(opts.sources, { name = "emoji" })
     end,
   },
   {
@@ -109,7 +108,7 @@ return {
       if not opts.sources then
         opts.sources = {}
       end
-      table.insert(opts.sources, { name = "latex_symbols", priority = 700 })
+      table.insert(opts.sources, { name = "latex_symbols" })
     end,
   },
   {
@@ -120,7 +119,7 @@ return {
       if not opts.sources then
         opts.sources = {}
       end
-      table.insert(opts.sources, { name = "nerdfont", priority = 700 })
+      table.insert(opts.sources, { name = "nerdfont" })
     end,
   },
   {
