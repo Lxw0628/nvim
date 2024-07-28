97bda2af2186e5d1f4af7e1ff4d0b3dada3b6187
Lxw0628 <761402785@qq.com>
2024-07-28 22:14:48 +0800

    change hop to falsh. and update plugins.

--- lua/plugins/flash.lua
+++ lua/plugins/flash.lua
@@ -2,9 +2,13 @@
 
 return {
   "folke/flash.nvim",
-  enabled = false,
+  enabled = true,
   keys = {
+    { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
     -- disable the default flash keymap
     { "S", mode = { "n", "x", "o" }, false },
+    { "r", mode = "o", false },
+    { "R", mode = { "o", "x" }, false },
+    { "<c-s>", mode = { "c" }, false },
   },
 }
--- lua/plugins/hop.lua
+++ lua/plugins/hop.lua
@@ -1,3 +1,5 @@
+if true then return {} end
+
 return {
   {
     "smoka7/hop.nvim",
