7c4de7d7a4c191a438097db64ee7e305b519ba4e
Lxw0628 <761402785@qq.com>
2024-08-01 22:34:22 +0800

    添加快捷键描述

--- lua/plugins/nvim-emmet.lua
+++ lua/plugins/nvim-emmet.lua
@@ -3,7 +3,7 @@ return {
     "olrtg/nvim-emmet",
     event = "VeryLazy",
     config = function()
-      vim.keymap.set({ "n", "v" }, "<leader>ce", require("nvim-emmet").wrap_with_abbreviation)
+      vim.keymap.set({ "n", "v" }, "<leader>ce", require("nvim-emmet").wrap_with_abbreviation, {desc = "Emmet wrap with abbreviation"})
     end,
   },
 }
