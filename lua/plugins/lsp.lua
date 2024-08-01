477ab8802dbab6a0dfb1ee1fad2dccc51494e7de
Lxw0628 <761402785@qq.com>
2024-08-01 22:35:24 +0800

    增加emmet_language_server lsp的设置 lang=>zh-CN

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -3,13 +3,60 @@ return {
   {
     "neovim/nvim-lspconfig",
     opts = {
-      codelens = {
-        enabled = true,
-      },
       setup = {
         clangd = function(_, opts)
           opts.capabilities.offsetEncoding = { "utf-16" }
         end,
+      },
+    },
+  },
+  -- emmet_language_server
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        emmet_language_server = {
+          filetypes = {
+            "css",
+            "eruby",
+            "html",
+            "javascript",
+            "javascriptreact",
+            "less",
+            "sass",
+            "scss",
+            "pug",
+            "typescriptreact",
+          },
+          -- Read more about this options in the [vscode docs](https://code.visualstudio.com/docs/editor/emmet#_emmet-configuration).
+          -- **Note:** only the options listed in the table are supported.
+          init_options = {
+            ---@type table<string, string>
+            includeLanguages = {},
+            --- @type string[]
+            excludeLanguages = {},
+            --- @type string[]
+            extensionsPath = {},
+            --- @type table<string, any> [Emmet Docs](https://docs.emmet.io/customization/preferences/)
+            preferences = {},
+            --- @type boolean Defaults to `true`
+            showAbbreviationSuggestions = true,
+            --- @type "always" | "never" Defaults to `"always"`
+            showExpandedAbbreviation = "always",
+            --- @type boolean Defaults to `false`
+            showSuggestionsAsSnippets = false,
+            --- @type table<string, any> [Emmet Docs](https://docs.emmet.io/customization/syntax-profiles/)
+            syntaxProfiles = {},
+            --- @type table<string, string> [Emmet Docs](https://docs.emmet.io/customization/snippets/#variables)
+            variables = {
+              lang = "zh-CN",
+              locale = "zh-CN",
+              charset = "UTF-8",
+              indentation = "\t",
+              newline = "\n",
+            },
+          },
+        },
       },
     },
   },
