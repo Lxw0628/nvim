3d0427f48302e133997a57bd4ca584cfd8991f3e
Lxw0628 <761402785@qq.com>
2024-08-14 20:21:40 +0800

    add something useful keybind

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -3,7 +3,7 @@
 -- Add any additional keymaps here
 
 local map = vim.keymap.set
-
+local opts = { noremap = true, silent = true }
 map("i", "jk", "<Esc>")
 map("i", "<C-h>", "<Left>")
 map("i", "<C-j>", "<Down>")
@@ -13,3 +13,12 @@ map({ "i", "n", "x" }, "<C-M-h>", "<Home>")
 map({ "i", "n", "x" }, "<C-M-j>", "<Down><Down><Down><Down><Down>")
 map({ "i", "n", "x" }, "<C-M-k>", "<Up><Up><Up><Up><Up>")
 map({ "i", "n", "x" }, "<C-M-l>", "<End>")
+
+map("n", "x", "_x")
+
+-- Increment/decrement
+map("n", "+", "<C-a>")
+map("n", "-", "<C-x>")
+
+-- Select all
+map("n", "<C-a>", "gg<S-v>G")
