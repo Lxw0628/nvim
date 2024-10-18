1546a907d33c60f5d5f5c86dadce15797205e0ec
Lxw0628 <761402785@qq.com>
2024-10-18 16:40:16 +0800

    ftplugin disabled use conform in vscode

--- after/ftplugin/javascript.lua
+++ after/ftplugin/javascript.lua
@@ -1,10 +1,12 @@
 vim.bo.tabstop = 4
 vim.bo.shiftwidth = 4
 
-require("conform").setup({
-    formatters = {
-        prettier = {
-            prepend_args = { "--tab-width", "4" },
+if not vim.g.vscode then
+    require("conform").setup({
+        formatters = {
+            prettier = {
+                prepend_args = { "--tab-width", "4" },
+            },
         },
-    },
-})
+    })
+end
--- after/ftplugin/lua.lua
+++ after/ftplugin/lua.lua
@@ -2,10 +2,12 @@ vim.bo.tabstop = 4
 vim.bo.shiftwidth = 4
 vim.bo.expandtab = true
 
-require("conform").setup({
-    formatters = {
-        stylua = {
-            prepend_args = { "--indent-width", "4", "--indent-type", "Spaces" },
+if not vim.g.vscode then
+    require("conform").setup({
+        formatters = {
+            stylua = {
+                prepend_args = { "--indent-width", "4", "--indent-type", "Spaces" },
+            },
         },
-    },
-})
+    })
+end
