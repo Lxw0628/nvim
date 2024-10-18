e4ad93d6cd2b9d3db529491059e7c02c118d948e
Lxw0628 <761402785@qq.com>
2024-10-18 16:23:02 +0800

    ftplugin html;tabstop=4,shiftwidth=4,prettier tab-width=4

--- after/ftplugin/html.lua
+++ after/ftplugin/html.lua
@@ -1,1 +1,12 @@
 -- vim.cmd("setlocal tabstop=4 shiftwidth=4")
+
+vim.bo.tabstop = 4
+vim.bo.shiftwidth = 4
+
+require("conform").setup({
+    formatters = {
+        prettier = {
+            prepend_args = { "--tab-width", "4" },
+        },
+    },
+})
