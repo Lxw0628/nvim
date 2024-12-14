90532e924f9e40091b0bb2e1af6ee245ab08eb29
Lxw0628 <761402785@qq.com>
2024-12-14 17:47:06 +0800

    refactor(plugins): Delete duplicate configuration files
    inlay_hints

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -26,7 +26,7 @@ return {
     {
         "neovim/nvim-lspconfig",
         opts = {
-            inlay_hints = { enabled = true },
+            inlay_hints = { enabled = false },
             -- ---@type lspconfig.options
             servers = {
                 html = {},
