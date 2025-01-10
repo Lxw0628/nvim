04df2bac6a7a3290940e67c79b86e72e92e3f2ca
Lxw0628 <761402785@qq.com>
2025-01-10 19:02:41 +0800

    style: use default configuration from LazyVim

--- lua/plugins/_blink.lua
+++ lua/plugins/_blink.lua
@@ -1,23 +1,12 @@
 return {
     {
         "saghen/blink.cmp",
-        dependencies = {},
         opts = {
             keymap = {
-                ["<C-space>"] = { "show", "show_documentation", "hide_documentation" },
-                ["<C-e>"] = { "hide", "fallback" },
-                ["<CR>"] = { "accept", "fallback" },
-
-                ["<Tab>"] = { "snippet_forward", "fallback" },
-                ["<S-Tab>"] = { "snippet_backward", "fallback" },
-
-                ["<Up>"] = { "select_prev", "fallback" },
-                ["<Down>"] = { "select_next", "fallback" },
-                ["<C-p>"] = { "select_prev", "fallback" },
-                ["<C-n>"] = { "select_next", "fallback" },
-
-                ["<C-b>"] = { "scroll_documentation_up", "fallback" },
-                ["<C-f>"] = { "scroll_documentation_down", "fallback" },
+                preset = "enter",
+                cmdline = {
+                    preset = "enter",
+                },
             },
         },
     },
