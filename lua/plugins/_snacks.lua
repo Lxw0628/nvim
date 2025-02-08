762696b5bf6b7c7917605c9c0972f6d247d3187a
Lxw0628 <761402785@qq.com>
2025-02-09 00:08:25 +0800

    update

--- lua/plugins/_snacks.lua
+++ lua/plugins/_snacks.lua
@@ -79,18 +79,18 @@ return {
     },
 
     -- LSP-integrated file renaming with support for plugin neo-tree.nvim
-    {
-        "nvim-neo-tree/neo-tree.nvim",
-        opts = function(_, opts)
-            local function on_move(data)
-                Snacks.rename.on_rename_file(data.source, data.destination)
-            end
-            local events = require("neo-tree.events")
-            opts.event_handlers = opts.event_handlers or {}
-            vim.list_extend(opts.event_handlers, {
-                { event = events.FILE_MOVED, handler = on_move },
-                { event = events.FILE_RENAMED, handler = on_move },
-            })
-        end,
-    },
+    -- {
+    --     "nvim-neo-tree/neo-tree.nvim",
+    --     opts = function(_, opts)
+    --         local function on_move(data)
+    --             Snacks.rename.on_rename_file(data.source, data.destination)
+    --         end
+    --         local events = require("neo-tree.events")
+    --         opts.event_handlers = opts.event_handlers or {}
+    --         vim.list_extend(opts.event_handlers, {
+    --             { event = events.FILE_MOVED, handler = on_move },
+    --             { event = events.FILE_RENAMED, handler = on_move },
+    --         })
+    --     end,
+    -- },
 }
