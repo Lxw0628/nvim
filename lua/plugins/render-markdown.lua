7eacc3c7fe9207085c8e056dcd5de61f499b278e
Lxw0628 <761402785@qq.com>
2025-02-17 16:13:13 +0800

    refactor(render-markdown): enabled checkbox, moved _markdown.lua to render-markdown.lua

--- lua/plugins/render-markdown.lua
+++ lua/plugins/render-markdown.lua
@@ -1,9 +1,25 @@
 return {
     {
+        -- https://github.com/MeanderingProgrammer/render-markdown.nvim
         "MeanderingProgrammer/render-markdown.nvim",
         opts = {
             bullet = {
                 right_pad = 1,
+            },
+            checkbox = {
+                -- https://github.com/MeanderingProgrammer/render-markdown.nvim/wiki/Checkboxes
+                enabled = true,
+                custom = {
+                    todo = { raw = "[-]", rendered = "󰥔 ", highlight = "RenderMarkdownTodo", scope_highlight = nil },
+                    important = { raw = "[~]", rendered = "󰓎 ", highlight = "DiagnosticWarn" },
+                },
+            },
+            heading = {
+                sign = true,
+                -- icons = { "󰲡 ", "󰲣 ", "󰲥 ", "󰲧 ", "󰲩 ", "󰲫 " },
+                signs = { "󰫎 " },
+                width = "block",
+                position = "inline",
             },
             html = {
                 comment = {
