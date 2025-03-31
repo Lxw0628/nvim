5f313cb517e0957997be4f045929cea35bc292c8
Lxw0628 <761402785@qq.com>
2025-03-31 16:37:41 +0800

    refactor(blink.cmp):  changed snippets score_offset = 0

--- lua/plugins/_blink.lua
+++ lua/plugins/_blink.lua
@@ -1,13 +1,16 @@
-if true then return {} end
+-- if true then return {} end
 
 return {
     {
         "saghen/blink.cmp",
         opts = {
-            keymap = {
-                preset = "enter",
-                cmdline = {
-                    preset = "enter",
+            sources = {
+                -- default = { "snippets", "lsp", "path", "buffer" },
+
+                providers = {
+                    snippets = {
+                        score_offset = 0,
+                    },
                 },
             },
         },
