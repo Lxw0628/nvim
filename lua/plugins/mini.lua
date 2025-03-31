e6f22e2a55d30b369fc9e38d493a56ccdf02f080
Lxw0628 <761402785@qq.com>
2025-03-31 16:36:24 +0800

    refactor(mini.snippets): changed keymap. c-j/k/h/l

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -1,3 +1,9 @@
+
+-- Set to `false` to prevent "non-lsp snippets"" from appearing inside completion windows
+-- Motivation: Less clutter in completion windows and a more direct usage of snippits
+vim.g.lazyvim_mini_snippets_in_completion = true
+
+
 return {
     {
         "echasnovski/mini.comment",
@@ -7,6 +13,17 @@ return {
                 comment_line = "<C-/>",
                 -- Toggle comment on visual selection
                 comment_visual = "<C-/>",
+            },
+        },
+    },
+    {
+        -- https://github.com/echasnovski/mini.nvim/blob/main/readmes/mini-snippets.md#expand
+        "echasnovski/mini.snippets",
+        opts = {
+            mappings = {
+                expand = "<C-'>",
+                jump_next = "<C-.>",
+                jump_prev = "<C-,>",
             },
         },
     },
