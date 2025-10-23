c27355b94170af962fdab404a4ea80a793b2420b
Lxw0628 <761402785@qq.com>
2025-10-23 19:04:37 +0800

    feat(avante.nvim): added provider "deepseek"

---
+++ lua/plugins/ai.lua
@@ -0,0 +1,16 @@
+return {
+    {
+        "yetone/avante.nvim",
+        opts = {
+            provider = "deepseek",
+            providers = {
+                deepseek = {
+                    __inherited_from = "openai",
+                    -- api_key_name = "DEEPSEEK_API_KEY",
+                    endpoint = "https://api.deepseek.com",
+                    model = "deepseek-coder",
+                },
+            },
+        },
+    },
+}
